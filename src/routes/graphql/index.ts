19cd4931cb5f492a5bfbb280c8089209f706a795
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-11-10 16:31:02 +0300

    feat: implement gql-rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { graphql, GraphQLSchema } from 'graphql';
+import { GraphQLSchema, execute, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { RootQueryType } from './types/query.js';
 import { Mutations } from './types/mutations.js';
@@ -11,18 +12,29 @@ const schema = new GraphQLSchema({
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
     schema: { ...createGqlResponseSchema, response: { 200: gqlResponseSchema } },
     async handler(req, reply) {
       const { query, variables } = req.body;
-      const result = await graphql({
+      const source = query;
+
+      const ast = parse(source);
+      const validationErrors = validate(schema, ast, [depthLimit(5)]);
+
+      if (validationErrors.length > 0) {
+        return reply.send({ errors: validationErrors });
+      }
+
+      const result = await execute({
         schema,
-        source: query,
+        document: ast,
         contextValue: { prisma },
         variableValues: variables,
       });
+
       return reply.send(result);
     },
   });
